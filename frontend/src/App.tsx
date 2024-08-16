d395ef7147bbbdc0b61b8872ae505a97acbbc20c
universeBuddy <rai358788@gmail.com>
2024-08-16 18:06:34 +0530

    file upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Genrate a video from a URL using power of AI </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat tempore
+        dolorum saepe dicta, ipsa, eaque, inventore aliquam rem adipisci laborum
+        incidunt dolore. Officia tempore hic error aperiam vel omnis ab?
+      </p>
+
+      <form>
+        <input type="url"></input>
+        <button type="submit"> Create Video</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
