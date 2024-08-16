dd89f45c2331676d0becdd0ce728bdd5b6d93bab
universeBuddy <rai358788@gmail.com>
2024-08-16 19:24:58 +0530

    UI structure complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,41 @@
-
-
 function App() {
   return (
     <>
-      <h1>Genrate a video from a URL using power of AI </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat tempore
-        dolorum saepe dicta, ipsa, eaque, inventore aliquam rem adipisci laborum
-        incidunt dolore. Officia tempore hic error aperiam vel omnis ab?
-      </p>
+      <main className="max-w-2xl mx-auto flex gap-8 px-4">
+        <div className="py-8 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold uppercase mb-4">
+            <span className="text-5xl bg-gradient-to-br from-emerald-500 to-sky-500 bg-clip-text text-transparent">
+              {" "}
+              URl to Viedo{" "}
+            </span>
+            <br />
 
-      <form>
-        <input type="url"></input>
-        <button type="submit"> Create Video</button>
-      </form>
+            <span className="bg-gradient-to-br from-emerald-500 to-sky-500 bg-clip-text text-transparent">
+              Using of AI
+            </span>
+          </h1>
+
+          <form className="grid gap-2">
+            <input
+              className="border-2 rounded-full bg-transparent text-white px-4 py-2 grow"
+              type="url"
+              placeholder="http://..."
+            />
+            <button
+              className=" rounded-full bg-emerald-500 text-white px-4 py-2 grow uppercase"
+              type="submit"
+            >
+              {" "}
+              Create&nbsp;Video
+            </button>
+          </form>
+        </div>
+        <div className="py-8">
+          <div className="bg-gray-200 w-[240px] h-[360px]  text-gray-500 rounded-2xl p-8">
+            Video Here
+          </div>
+        </div>
+      </main>
     </>
   );
 }
