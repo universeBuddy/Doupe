9d512c92df94040e1795b9de12372149dd849b31
universeBuddy <rai358788@gmail.com>
2024-08-17 20:46:41 +0530

    form feild complete

--- frontend/src/components/FormField.jsx
+++ frontend/src/components/FormField.jsx
@@ -20,14 +20,26 @@ const FormField = ({
           {LableName}
         </lable>
 
-        {isSupriseMe &&(
-          <button type="button" onClick={handleSupriseMe} className=" font-semibold text-xs bg-[#ECECF1]  py-1 px-2 rounded-[5px] text-black>
+        {isSupriseMe && (
+          <button
+            type="button"
+            onClick={handleSupriseMe}
+            className=" font-semibold text-xs bg-[#ECECF1]  py-1 px-2 rounded-[5px] text-black"
+          >
+            Suprise Me
           </button>
-
-        )
-
-        }
+        )}
       </div>
+
+      <input
+        type={type}
+        id={name}
+        placeholder="placeholder"
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
     </div>
   );
 };
--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FormField } from "../components";
+import { FormField, Loader } from "../components";
+import { preview } from "../assets";
 
 const CreatePost = () => {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ const CreatePost = () => {
   const [genratingImg, setGeratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const genrateImage = () => {};
   const handleSubmit = () => {};
   const handleChange = (e) => {};
   const handleSupriseMe = () => {};
@@ -46,7 +48,48 @@ const CreatePost = () => {
             isSupriseMe
             handleSupriseMe={handleSupriseMe()}
           />
-        </div> 
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt={preview}
+                className="w-9/12h-9/12 object-contain opacity-40 "
+              />
+            )}
+
+            {genratingImg && (
+              <div className="absoulte inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className=" mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={genrateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {genratingImg ? "Genrating..." : "Genrate Image"}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 tetx-[#666e75] tetx-[14px]">
+            Once you have created the image you want, you cab share it with
+            others in Community
+          </p>
+
+          <button type="submit" className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">Share with the Community</button>
+        </div>
       </form>
     </section>
   );
