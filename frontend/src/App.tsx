932a4a86203a9ec22edad05d4ddce7a323eb2577
universeBuddy <rai358788@gmail.com>
2024-08-17 16:11:25 +0530

    creating the card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,33 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+import { CreatePost, Home } from "./pages";
+
+const App = () => {
   return (
-    <>
-      <main className="max-w-2xl mx-auto flex gap-8 px-4">
-        <div className="py-8 flex flex-col justify-center">
-          <h1 className="text-4xl font-bold uppercase mb-4">
-            <span className="text-5xl bg-gradient-to-br from-emerald-500 to-sky-500 bg-clip-text text-transparent">
-              {" "}
-              URl to Viedo{" "}
-            </span>
-            <br />
+    <BrowserRouter>
+      <header className="w full flex justify-between items-center bg-white sm:px-8 px-4 py-4 bordder-b-[#e6ebf4]   ">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
 
-            <span className="bg-gradient-to-br from-emerald-500 to-sky-500 bg-clip-text text-transparent">
-              Using of AI
-            </span>
-          </h1>
+        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md "> CReate</Link>
+      </header>
 
-          <form className="grid gap-2">
-            <input
-              className="border-2 rounded-full bg-transparent text-white px-4 py-2 grow"
-              type="url"
-              placeholder="http://..."
-            />
-            <button
-              className=" rounded-full bg-emerald-500 text-white px-4 py-2 grow uppercase"
-              type="submit"
-            >
-              {" "}
-              Create&nbsp;Video
-            </button>
-          </form>
-        </div>
-        <div className="py-8">
-          <div className="bg-gray-200 w-[240px] h-[360px]  text-gray-500 rounded-2xl p-8">
-            Video Here
-          </div>
-        </div>
+
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]  ">
+
+
+
+        <Routes>
+          <Route path="/" element={<Home /> } />
+          <Route path="/create-post" element={<CreatePost /> } />
+
+         
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
