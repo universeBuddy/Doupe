fb2555d7116ff38929a19fdbb0a5bc11ed8becac
universeBuddy <rai358788@gmail.com>
2024-08-17 14:59:14 +0530

    connecting with gpt script

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,17 @@ import express from "express"
 const app =express()
 
 app.get('/test',(req,res)=>{
-return res.json("ok but not strd")
+return res.json("ok but not stssrd");
+
+})
+
+app.get('/create-story',(req,res)=>{
+
+    const url = req.query.url;
+
+    console.log({url})
+return res.json('ok')
+
 
 })
 app.listen(8080,()=> console.log("Listen at 8080"))
